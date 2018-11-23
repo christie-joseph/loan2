cd337d7fe887b1c40881af689b2a4161a25f0718
cjcr7 <45203035+cjcr7@users.noreply.github.com>
2018-11-23 22:52:26 +0530

    Add files via upload

---
+++ loan_app/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth.models import User
+from loan_app.models import UserLoanInfo
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password=forms.CharField(widget=forms.PasswordInput())
+
+    class Meta():
+        model = User
+        fields = ('first_name','last_name','email','username','password')
+
+    def clean(self):
+        cleaned_data = super(UserForm, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "password and confirm_password does not match"
+            )
+
+class UserLoanInfoForm(forms.ModelForm):
+    class Meta():
+        model = UserLoanInfo
+        fields = ('address',)  #add loan1,loan2,loan3
---
+++ loan_app/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from loan_app import views
+from django.urls import path
+
+app_name = 'loan_app'
+
+urlpatterns =[
+    path('login/',views.user_login,name='user_login'),
+    path('signup/',views.signup,name='signup'),
+    #path('reg_success/',views.)
+
+]
